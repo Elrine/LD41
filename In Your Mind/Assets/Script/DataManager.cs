be39051a31a1d492a39dad7e7c780a7d604a161d
Nathan Harlé <nathan.harle@epitech.eu>
2018-04-21 11:30:27 +0200

    first commit

---
+++ In Your Mind/Assets/Script/DataManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.SceneManagement;
+
+public class DataManager : MonoBehaviour
+{
+
+    private static bool _created = false;
+    private Flowchart Flowchart;
+    private bool result;
+    private String sceneName;
+
+    void Awake()
+    {
+        if (!_created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            _created = true;
+        }
+    }
+    
+
+    public void loadVisualNovelScene(bool _result, string _sceneName)
+    {
+        result = _result;
+        sceneName = _sceneName;
+        StartCoroutine(loadScene());
+    }
+
+    IEnumerator loadScene()
+    {
+        var asyncLoadScene = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoadScene.isDone)
+        {
+            yield return null;
+        }
+
+        Flowchart flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+        Debug.Log(flowchart.HasBlock("onWin"));
+        flowchart.ExecuteBlock((result) ? "onWin" : "onLose");
+    }
+}
---
+++ In Your Mind/Assets/tmp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class tmp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Flowchart flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+	    Debug.Log("tmp:" + flowchart.HasBlock("onWin"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
